168f33c2080da745594bc6530016cd8248572b4a
Estácio Ramalho <estacioramalhodev@gmail.com>
2023-12-06 13:44:58 -0300

    Week 2

---
+++ Week 2 - Arrays/Problem Set 2/readability.c
@@ -0,0 +1,63 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+
+int main(void)
+{
+    // Takes a text as input and calculates the grade
+    string text = get_string("Enter your text: ");
+    int grade = count_letters(text);
+
+    // Prints the grade
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+int count_letters(string text)
+{
+    int textLenght = strlen(text);
+
+    float letterCount = 0;
+    float wordCount = 1;
+    float sentenceCount = 0;
+
+    // Loops through each character in the text to count letters, words, and sentences
+    for (int character = 0; character < textLenght; character++)
+    {
+        char currentCharacter = text[character];
+
+        if ((currentCharacter >= 'a' && currentCharacter <= 'z') || (currentCharacter >= 'A' && currentCharacter <= 'Z'))
+        {
+            letterCount++;
+        }
+        else if (currentCharacter == ' ')
+        {
+            wordCount++;
+        }
+        else if (currentCharacter == '.' || currentCharacter == '!' || currentCharacter == '?')
+        {
+            sentenceCount++;
+        }
+    }
+
+    // Calculates the grade using the Coleman-Liau index
+    float avarageLetters = letterCount / wordCount * 100;
+    float avarageSentences = sentenceCount / wordCount * 100;
+
+    int grade = round(0.0588 * avarageLetters - 0.296 * avarageSentences - 15.8);
+
+    return grade;
+}
